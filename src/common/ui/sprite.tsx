38b10963877a40c078591b17cfe0d985b3ad80e7
Ciel <ciel@MacBook-Air-Ciel.local>
2024-10-30 23:02:16 +0400

    change sprite

--- src/common/ui/sprite.tsx
+++ src/common/ui/sprite.tsx
@@ -1,17 +1,40 @@
+import Image from "next/image";
+
 type IconProps = {
-    name: string
-    className: string
-    onClick?: any
-}
+    name: string;
+    className: string;
+    onClick?: () => void;
+    isImage?: boolean;
+    style?: React.CSSProperties;
+    isPng?: boolean;
+};
 
 export function Sprite({
     name,
     className,
-    onClick
+    onClick,
+    isImage,
+    style, 
+    isPng,
 }: IconProps): JSX.Element {
+    if (isImage) {
+        return (
+            <Image
+                fetchPriority="high"
+                src={`/icons/${name}.${isPng ? "png" : "svg"}`}
+                alt=""
+                width={256}
+                height={256}
+                className={className}
+                onClick={onClick}
+                style={style}
+            />
+        );
+    }
+
     return (
-        <svg onClick={onClick} className={className}>
+        <svg onClick={onClick} className={className} style={style}>
             <use xlinkHref={`/sprite.svg#${name}`} />
         </svg>
-    )
+    );
 }
